becf0c91451e0e60b6c0fafc919d5f8fb09b38b6
ListraiD <60462694+ListraiD@users.noreply.github.com>
2023-04-07 15:51:58 +0300

    Создание макета
    Закинул тестовые файлы

---
+++ index.php
@@ -0,0 +1,87 @@
+<!doctype html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+    <title>Landing</title>
+    <style>
+        .btn-header {
+            border: 1px solid gray;
+            border-radius: 5px;
+        }
+
+        .numbers {
+            color: #D5D4D4;
+            font-size: 144px;
+            font-weight: 500;
+            text-align: center;
+        }
+
+    </style>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <div class="row">
+            <div class="col-5" style="height: 100vh;">
+                <div class="row" style="height: 100%;">
+                    <div class="col-10 mx-auto" style="height: 100%;">
+                        <div class="row" style="height: 100%;">
+                            <div class="col my-auto">
+                                <div class="row">
+                                    <h1 style="font-size: 70px; font-weight: 100;">LOREM IPSUM ONE PAGE ONLY</h1>
+                                </div>
+                                <div class="row mt-2">
+                                    <p style="font-size: 24px; font-weight: 200;">LOREM IPSUM ONE PAGE ONLY</p>
+                                </div>
+                                <div class="row">
+
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col bg-dark">
+
+            </div>
+        </div>
+
+        <div class="row" style="height: 100vh;background:#D5D4D4;">
+            <div class="col">
+                <div class="row" style="height: 100%;">
+                    <div class="col-6 mx-auto my-auto">
+                        <div class="row">
+                            <h1 style="font-size: 70px; font-weight: 300;">
+                                LOREM IPSUM KOKA KRYLOV
+                            </h1>
+                        </div>
+                        <div class="row">
+                            <p style="font-size: 24px; font-weight: 200;text-align:center">LOREM IPSUM ONE PAGE ONLY</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+            <div class="col">
+                <div class="row" style="height:100vh;">
+                    <div class="col-4 mx-auto my-auto" style="height: 70vh;background: #605E5E;">
+                        <p class="numbers">1</p>
+                    </div>
+                    <div class="col-4 mx-auto my-auto" style="height: 70vh;background: #605E5E;">
+                        <p class="numbers">2</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+</body>
+
+</html>
